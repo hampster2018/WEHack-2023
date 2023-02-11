d89e06a97b52a92516e875cc54af0f1c820636ad
Nicholas Zolton <78943323+NicholasZolton@users.noreply.github.com>
2023-02-11 12:15:17 -0600

    Added welcome page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <main>
-	<h1>Welcome to SvelteKit</h1>
+	<h1>Welcome to our Project</h1>
 	<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 </main>
 
