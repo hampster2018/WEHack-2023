8e6daef1828688eb6f474259d4ae4fe47e0c5dc5
Nicholas Zolton <78943323+NicholasZolton@users.noreply.github.com>
2023-02-11 14:28:43 -0600

    Working map

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,17 @@
 <script>
-
+	import Map from './Map.svelte';
 </script>
 
 <main>
-	<h1>Welcome to our Project</h1>
-	<p>Visit <a href="https://github.com/hampster2018/WEHack-2023">our Github repository</a> to check out more about our project!</p>
+	<div id="welcome" class="fullscreen-page">
+		<h1>Welcome to our Project</h1>
+		<p>Visit <a href="https://github.com/hampster2018/WEHack-2023">our Github repository</a> to check out more about our project!</p>
+	</div>
+
+	<div id="map" class="fullscreen-page">
+		<Map />
+	</div>
+
 </main>
 
 <style>
@@ -12,6 +19,7 @@
 	/* Remove the margin so the page is full height */
 	:global(body) {
 		margin: 0;
+		padding: 0;
 	}
 
 	/* Make the text in the header more nice */
@@ -22,13 +30,13 @@
 	}
 
 	/* Center the header and paragraph */
-	main {
+	.fullscreen-page {
 		display: flex;
 		flex-direction: column;
 		align-items: center;
 		justify-content: center;
 		height: 100vh;
-		width: 100vw;
+		width: 100%;
 		text-align: center;
 	}
 
---
+++ src/routes/Map.svelte
@@ -0,0 +1,37 @@
+<script>
+	import { Loader } from '@googlemaps/js-api-loader';
+
+    /**
+	 * @type {HTMLDivElement}
+	 */
+    let container;
+    let map;
+    let zoom = 8;
+    let center = { lat: 32.7916841, lng: -96.804615};
+
+
+    const loader = new Loader({
+        apiKey: "",
+    });
+
+    const mapOptions = {
+        center: center,
+        zoom: zoom,
+    };
+
+    loader.load().then((google) => {
+        new google.maps.Map(container, mapOptions);
+    }).catch(e => {
+        console.error(e);
+    });
+
+</script>
+
+<style>
+.full-screen {
+    width: 100%;
+    height: 100%;
+}
+</style>
+
+<div class="full-screen" bind:this={container}></div>
