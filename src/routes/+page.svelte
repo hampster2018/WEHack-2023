9f74a8ec488acc7242a1d54a66aa779b47251d2c
Nicholas Zolton <78943323+NicholasZolton@users.noreply.github.com>
2023-02-11 12:13:51 -0600

    Added basic intro page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,35 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+
+</script>
+
+<main>
+	<h1>Welcome to SvelteKit</h1>
+	<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+</main>
+
+<style>
+
+	/* Remove the margin so the page is full height */
+	:global(body) {
+		margin: 0;
+	}
+
+	/* Make the text in the header more nice */
+	h1 {
+		font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+		font-size: 3rem;
+		font-weight: 300;
+	}
+
+	/* Center the header and paragraph */
+	main {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		height: 100vh;
+		width: 100vw;
+		text-align: center;
+	}
+
+</style>
